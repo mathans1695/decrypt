15e976a92bdea5a5c70ef4d3da8025fed98794e0
Mahalingam Sundararaj <mathans1695@gmail.com>
2021-01-04 13:55:22 +0530

    create letters clock with the help of Circular Linked List

---
+++ LettersClock.py
@@ -0,0 +1,10 @@
+from CircularLL import Root	
+
+letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		   'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+		   'U', 'V', 'W', 'X', 'Y', 'Z']
+
+letters_clock = Root(letters[0])
+
+for i in range(1, len(letters)):
+	letters_clock.insert('', letters[i])
