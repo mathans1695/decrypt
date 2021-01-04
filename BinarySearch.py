91c01e61e28a0a4fa1acd98d55a593cc990ee760
Mahalingam Sundararaj <mathans1695@gmail.com>
2021-01-04 19:19:40 +0530

    implement binary search on list for better result

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,26 +1,25 @@
+from LettersClock import letters
+
 def binary_search(arr, value):
 	length = len(arr)
 	mid = length // 2
 	
-	try:
-		arr[mid]
-	except IndexError:
-		return 0
+	if mid == 0:
+		if arr[mid] == value.upper():
+			return mid
+		return None
 	
-	if arr[mid] == value:
+	if arr[mid] == value.upper():
 		return mid
 	
-	elif arr[mid] > value:
+	elif arr[mid] > value.upper():
 		return binary_search(arr[:mid], value)
 	
 	else:
 		return mid + binary_search(arr[mid+1:], value) + 1
 
 def find_index(arr, value):
-	index = binary_search(arr, value)
-	length = len(arr)
-	
-	if(length == index):
+	try:
+		return binary_search(arr, value)
+	except TypeError:
 		return None
-	else:
-		return index
