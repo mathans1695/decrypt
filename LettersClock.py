fb933e57e0e8e7159865047dc7be5cb5f1590d21
Mahalingam Sundararaj <mathans1695@gmail.com>
2021-01-04 20:12:21 +0530

    change letters list into string

--- LettersClock.py
+++ LettersClock.py
@@ -1,8 +1,6 @@
 from CircularLL import Root	
-
-letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-		   'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-		   'U', 'V', 'W', 'X', 'Y', 'Z']
+		   
+letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 letters_clock = Root(letters[0])
 
