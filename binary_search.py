4a1708eba39f7aef346700b7734bfd34f96726a4
Mahalingam Sundararaj <mathans1695@gmail.com>
2021-01-05 10:12:52 +0530

    add comments

--- binary_search.py
+++ binary_search.py
@@ -1,6 +1,10 @@
 from letters_clock import letters
 
 def binary_search(arr, value):
+	"""Perform binary_search on arr using value
+	   Returns index, if value present in the list
+	   Throw TypeError, if value not present in the list"""
+			
 	length = len(arr)
 	mid = length // 2
 	
@@ -19,6 +23,8 @@ def binary_search(arr, value):
 		return mid + binary_search(arr[mid+1:], value) + 1
 
 def find_index(arr, value):
+	"""Returns the index of value from arr passed as parameters"""
+	
 	try:
 		return binary_search(arr, value)
 	except TypeError:
